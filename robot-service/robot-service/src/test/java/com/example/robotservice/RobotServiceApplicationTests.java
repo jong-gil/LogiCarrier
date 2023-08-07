2ec3b5c78504baa381238c2a9ee27e3d2080489b
boham97 <kangbosung97@gmail.com>
2023-08-08 00:27:53 +0900

    [FEAT] robot-service init

---
+++ robot-service/robot-service/src/test/java/com/example/robotservice/RobotServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.robotservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RobotServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
