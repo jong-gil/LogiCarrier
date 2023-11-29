d6950bce3198faee43f7e07e30fcba221fd8666f
jkjk0903 <jkjk0903@gmail.com>
2023-11-29 14:22:20 +0900

    [CHORE] Eureka 설정 및 health_check method 추가

--- worker-service/src/main/java/com/example/workerservice/WorkerServiceApplication.java
+++ worker-service/src/main/java/com/example/workerservice/WorkerServiceApplication.java
@@ -2,8 +2,11 @@ package com.example.workerservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class WorkerServiceApplication {
 
     public static void main(String[] args) {
