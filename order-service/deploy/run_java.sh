9dbdd1ed32a45f4ebb01a1421399c537fabdcb83
boham97 <kangbosung97@gmail.com>
2023-12-14 02:08:28 +0900

    [FEAT] codedeploy 스크립트 추가

---
+++ order-service/deploy/run_java.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /opt/cdtest
+version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
+service=$(echo *.jar | cut -d'-' -f1,2)
+
+sudo docker build -t $service:$version .
+echo "build"
+sudo docker run -d --name "$service-1" $service:$version
+echo "successfully run!"
+sudo docker stop "$service-2"
+sudo docker rm "$service-2"
