38d61b96fc51962e0e0c20a5ff98f7b518d59871
boham97 <kangbosung97@gmail.com>
2023-12-14 22:57:36 +0900

    [FIX] 포트 추가

--- discovery-service/deploy/kill_process.sh
+++ discovery-service/deploy/kill_process.sh
@@ -5,7 +5,7 @@ service=$(echo *.jar | cut -d'-' -f1,2)
 
 sudo docker build -t $service:$version .
 echo "build"
-sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier" $service:$version
+sudo docker run -d --name "$service-2" -p 8761:8761 --network logicarrier-network $service:$version
 echo "successfully run!"
 sudo docker stop "$service-1"
 sudo docker rm "$service-1"
