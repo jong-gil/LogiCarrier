50caa1e716c849eca841da7295728b82d0f9cd8d
boham97 <kangbosung97@gmail.com>
2023-12-14 22:54:22 +0900

    [FIX] eureka service삭제

--- eureka/deploy/kill_process.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-sudo docker stop discovery-service
-sudo docker rm discovery-service
-sudo docker image prune
--- eureka/deploy/run_java.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-cd /opt/discovery-service
-echo "배포"
-version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-echo $version
-sudo docker build -t discovery-service:$version .
-echo "build"
-sudo docker run -d -p 8761:8761 --name discovery-service discovery-service:$version
-echo "successfully run!"
