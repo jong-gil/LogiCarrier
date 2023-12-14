b39226d033c8bcfc54bf0deeadd500d2f11807e0
boham97 <kangbosung97@gmail.com>
2023-12-14 22:51:33 +0900

    [FEAT] discovery-service deploy

--- discovery-service/deploy/run_java.sh
+++ discovery-service/deploy/run_java.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
-cd /opt/cdtest
+cd /opt/discovery-service
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
 service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker run -d --name "$service-1" $service:$version
+sudo docker run -d --name "$service-1" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier" $service:$version
 echo "successfully run!"
 sudo docker stop "$service-2"
 sudo docker rm "$service-2"
