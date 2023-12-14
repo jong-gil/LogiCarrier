5f8811a0de3534c0acd27550a5e01e5aa8d528c4
boham97 <kangbosung97@gmail.com>
2023-12-14 23:10:28 +0900

    [FEAT] 환경변수 추가

--- order-service/deploy/kill_process.sh
+++ order-service/deploy/kill_process.sh
@@ -5,7 +5,12 @@ service=$(echo *.jar | cut -d'-' -f1,2)
 
 sudo docker build -t $service:$version .
 echo "build"
-sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier" $service:$version
+sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/"\
+ -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier"\
+ -e "spring_redis_host: redis"\
+ -e "spring_kafka_host: kafka1"\
+ -e "spring_kafka_port: 19092"\
+  $service:$version
 echo "successfully run!"
 sudo docker stop "$service-1"
 sudo docker rm "$service-1"
--- worker-service/deploy/run_java.sh
+++ worker-service/deploy/run_java.sh
@@ -3,7 +3,12 @@ cd /opt/worker-service
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
 service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker run -d --name "$service-1"  --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier" $service:$version
+sudo docker run -d --name "$service-1"  --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/"\
+ -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier"\
+ -e "spring_redis_host: redis"\
+ -e "spring_kafka_host: kafka1"\
+ -e "spring_kafka_port: 19092"\
+  $service:$version
 echo "successfully run!"
 sudo docker stop "$service-2"
 sudo docker rm "$service-2"
