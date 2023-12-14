0be3fc915fa24bbe0a6ddb26975c74f629f62a01
boham97 <kangbosung97@gmail.com>
2023-12-14 22:40:04 +0900

    [FEAT] order-service deploy 추가

--- order-service/deploy/kill_process.sh
+++ order-service/deploy/kill_process.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
-cd /opt/cdtest
+cd /opt/order-service
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
 service=$(echo *.jar | cut -d'-' -f1,2)
 
 sudo docker build -t $service:$version .
 echo "build"
-sudo docker run -d --name "$service-2" $service:$version
+sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" -e "spring.datasource.url=jdbc:mysql://mysql:3306/logiCarrier" $service:$version
 echo "successfully run!"
 sudo docker stop "$service-1"
 sudo docker rm "$service-1"
