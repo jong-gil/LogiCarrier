52350a3f89c8b5d6d1470e8cdd80535c6b290d52
boham97 <kangbosung97@gmail.com>
2023-12-15 18:05:04 +0900

    [FIX] 로그인 오더 로봇 서비스 deploy 수정

--- order-service/deploy/run_java.sh
+++ order-service/deploy/run_java.sh
@@ -3,6 +3,18 @@ cd /opt/order-service
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
 service=$(echo *.jar | cut -d'-' -f1,2)
 
+sudo docker build -t $service:$version .
+echo "build"
+sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
+ -e "spring.datasource.url=jdbc:mysql://logicarrier-db:3306/delivery" \
+ -e "spring_redis_host: redis" \
+ -e "spring_kafka_host: kafka1" \
+ -e "spring_kafka_port: 19092" \
+  $service:$version
+echo "successfully run!"
+sudo docker stop "$service-1"
+sudo docker rm "$service-1"
+
 sudo docker run -d --name "$service-1" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
  -e "spring.datasource.url=jdbc:mysql://logicarrier-db:3306/delivery" \
  -e "spring_redis_host: redis" \
