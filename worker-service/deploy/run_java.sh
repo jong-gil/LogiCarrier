95d21db3d9275f77b50235f8308459b3811ce7ea
boham97 <kangbosung97@gmail.com>
2023-12-15 17:52:02 +0900

    [FIX] db컨테이너 이름 수정

--- worker-service/deploy/run_java.sh
+++ worker-service/deploy/run_java.sh
@@ -6,7 +6,7 @@ service=$(echo *.jar | cut -d'-' -f1,2)
 sudo docker build -t $service:$version .
 echo "build"
 sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
- -e "spring.datasource.url=jdbc:mysql://mysql:3306/picker" \
+ -e "spring.datasource.url=jdbc:mysql://logicarrier-db:3306/picker" \
  -e "spring_redis_host: redis" \
  -e "spring_kafka_host: kafka1" \
  -e "spring_kafka_port: 19092" \
@@ -17,7 +17,7 @@ sudo docker rm "$service-1"
 sudo docker image prune
 
 sudo docker run -d --name "$service-1"  --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
- -e "spring.datasource.url=jdbc:mysql://mysql:3306/picker" \
+ -e "spring.datasource.url=jdbc:mysql://logicarrier-db:3306/picker" \
  -e "spring_redis_host: redis" \
  -e "spring_kafka_host: kafka1" \
  -e "spring_kafka_port: 19092" \
