7201755373b7dcec672e18a41bf8615c37725e54
boham97 <kangbosung97@gmail.com>
2023-12-15 15:12:40 +0900

    [FIX] discovery-service 스크립트 파일 수정

--- discovery-service/deploy/kill_process.sh
+++ discovery-service/deploy/kill_process.sh
@@ -1,12 +1,7 @@
 #!/bin/bash
 cd /opt/discovery-service
-version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker build -t $service:$version .
-echo "build"
-sudo docker run -d --name "$service-2" -p 8761:8761 --network logicarrier-network $service:$version
-echo "successfully run!"
-sudo docker stop "$service-1"
-sudo docker rm "$service-1"
-sudo docker image prune
+
+sudo docker stop discovery-service
+sudo docker rm discovery-service
+sudo docker image rm discovery-service
--- discovery-service/deploy/run_java.sh
+++ discovery-service/deploy/run_java.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /opt/discovery-service
-version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker run -d --name "$service-1" -p 8761:8761 --network logicarrier-network $service:$version
+sudo docker build -t discovery-service .
+sudo docker run -d --name discovery-service -p 8761:8761 --network logicarrier-network discovery-service
 echo "successfully run!"
-sudo docker stop "$service-2"
-sudo docker rm "$service-2"
