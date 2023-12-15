0c5b4b940e73f9b5227b842d97ef8dfbcf12969d
boham97 <kangbosung97@gmail.com>
2023-12-15 17:35:02 +0900

    [FIX] 워커서비스 deploy 수정

--- worker-service/deploy/run_java.sh
+++ worker-service/deploy/run_java.sh
@@ -3,6 +3,19 @@ cd /opt/worker-service
 version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
 service=$(echo *.jar | cut -d'-' -f1,2)
 
+sudo docker build -t $service:$version .
+echo "build"
+sudo docker run -d --name "$service-2" --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
+ -e "spring.datasource.url=jdbc:mysql://mysql:3306/picker" \
+ -e "spring_redis_host: redis" \
+ -e "spring_kafka_host: kafka1" \
+ -e "spring_kafka_port: 19092" \
+  $service:$version
+echo "successfully run!"
+sudo docker stop "$service-1"
+sudo docker rm "$service-1"
+sudo docker image prune
+
 sudo docker run -d --name "$service-1"  --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" \
  -e "spring.datasource.url=jdbc:mysql://mysql:3306/picker" \
  -e "spring_redis_host: redis" \
