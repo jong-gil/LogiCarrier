25e882a10be69ffff8c5938bedef104ceb46ede1
boham97 <kangbosung97@gmail.com>
2023-12-15 15:26:39 +0900

    [FIX] 게이트웨이 서비스 스크립트 변경

--- gateway-service/deploy/kill_process.sh
+++ gateway-service/deploy/kill_process.sh
@@ -1,12 +1,6 @@
 #!/bin/bash
 cd /opt/gateway-service
-version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker build -t $service:$version .
-echo "build"
-sudo docker run -d --name "$service-2" -p 8000:8000 --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" $service:$version
-echo "successfully run!"
-sudo docker stop "$service-1"
-sudo docker rm "$service-1"
-sudo docker image prune
+sudo docker stop gateway-service
+sudo docker rm gateway-service
+sudo docker image rm gateway-service
--- gateway-service/deploy/run_java.sh
+++ gateway-service/deploy/run_java.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 cd /opt/gateway-service
-version=$(echo *.jar | grep -oP '\d+\.\d+\.\d+')
-service=$(echo *.jar | cut -d'-' -f1,2)
 
-sudo docker run -d --name "$service-1" -p 8000:8000 --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" $service:$version
+sudo docker build -t gateway-service .
+sudo docker run -d --name gateway-service -p 8080:8080 --network logicarrier-network -e "eureka.client.serviceUrl.defaultZone=http://discovery-service:8761/eureka/" gateway-service
 echo "successfully run!"
-sudo docker stop "$service-2"
-sudo docker rm "$service-2"
